34feaeda57912d51efd8b092efde6938f596d4c1
trueLoving <starsky.zhanglinjie@gmail.com>
2022-06-28 21:19:02 +0800

    feat: add background canvas:Sakura

--- packages/lolimeow-theme/Layout.vue
+++ packages/lolimeow-theme/Layout.vue
@@ -7,13 +7,13 @@
   <SearchBox />
 </template>
 <script setup>
+import { onMounted } from "vue";
+
 import Home from "./views/Home.vue";
 import Footer from "./views/Footer.vue";
 import GoTop from "./components/GoTop/index.vue";
 import SearchBox from "./components/SearchBox/index.vue";
-// import Loading from './components/PageLoading/index.vue'
-// TODO: 打包环境区分
-// if (import.meta.env.SSR) {
-// import("./components/Sakura/index.js");
-// }
+onMounted(() => {
+  import("./components/Sakura/index.js");
+});
 </script>
