40fac6b3355fb91cfcd2b30162c24d167cde9982
trueLoving <starsky.zhanglinjie@gmail.com>
2022-07-03 21:42:29 +0800

    feat: update README.MD

--- www/.vitepress/config.ts
+++ www/.vitepress/config.ts
@@ -2,6 +2,8 @@ import { defineConfigWithTheme } from 'vitepress';
 import { LWThemeConfig } from 'vitepress-lolimeow-theme';
 import * as themeConfig from './data'
 
+
+
 export default defineConfigWithTheme<LWThemeConfig>({
   title: 'Wordpress lolimeow Theme Fork',
   description: 'Wordpress lolimeow Theme Fork',
